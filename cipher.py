bb47ff5416abe8cb2b05ebc8a82656c1639bb0c4
Bakief04 <65417132+Bakief04@users.noreply.github.com>
2024-08-12 19:18:58 -0400

    finished

--- cipher.py
+++ cipher.py
@@ -1,1 +1,16 @@
 # add your code here
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+prompt = input("Enter a message: ")
+shift = int(+5)
+length = len(prompt)
+
+output = ""
+
+for x in range(length):
+    letter = prompt[x]
+    location = alphabet.find(letter)
+    shift_location = (location + shift) % 26
+    output += alphabet[shift_location]
+    
+print(output)
